1818dbf51b30a48b63d16800985e11d823a8dca1
Leonard-Ta <75407373+Leonard-Ta@users.noreply.github.com>
2021-03-01 22:45:45 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket11"
+    bucket = "terraform-state-bucket111"
     key    = "path/path/terraform.tfstate"
     region = "us-east-1"
     profile = "default"
